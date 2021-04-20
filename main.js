c5d594b814d4cad95716909ba76e341d15cd19b3
Olivia Wissig <oliviawissig@gmail.com>
2021-04-20 15:23:49 -0700

    added c/del keys on top, and period bottom below

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
-const symbols = ['7', '8', '9', '÷', '4', '5', '6', '×', '3', '2', '1', '-', 'C', '0', '=', '+'];
+const symbols = ['7', '8', '9', '÷', '4', '5', '6', '×', '3', '2', '1', '-', '.', '0', '=', '+'];
+const topContainer = document.querySelector('#top-container');
+const container = document.querySelector('#btn-container');
 
 function init(){
     let counter = 0;
 
-    const container = document.querySelector('#btn-container');
+    let clearBtnText = document.createElement('p');
+    clearBtnText.classList.add('btn-text');
+    clearBtnText.textContent = 'C';
+
+    let clearBtn = document.createElement('div');
+    clearBtn.classList.add('top-square');
+    clearBtn.setAttribute('id', 'C');
+    clearBtn.style.backgroundColor = "#443850";
+
+    clearBtn.appendChild(clearBtnText);
+    topContainer.append(clearBtn);
+
+    let delBtnText = document.createElement('p');
+    delBtnText.classList.add('btn-text');
+    delBtnText.textContent = 'Del';
+
+    let delBtn = document.createElement('div');
+    delBtn.classList.add('top-square');
+    delBtn.setAttribute('id', 'Del');
+    delBtn.style.backgroundColor = "#443850";
+
+    delBtn.appendChild(delBtnText);
+    topContainer.append(delBtn);
+
     for(let i = 0; i < 4; i++){
         for(let j=0; j < 4; j++){
             let btnText = document.createElement('p');
             btnText.classList.add('btn-text');
-            btnText.style.marginTop = "16px";
-            btnText.style.textAlign = "center";
-            btnText.style.fontSize = "34px";
             btnText.textContent = symbols[counter];
 
             let newBtn = document.createElement('div');
             newBtn.classList.add('square');
             newBtn.setAttribute('id', symbols[counter]);
             newBtn.style.backgroundColor = "#C0AB8E";
-            newBtn.style.height = "80px";
-            if(symbols[counter] == 'C'){
+            if(symbols[counter] == '.'){
                 newBtn.style.backgroundColor = "#BE9EC9";
             }else if(symbols[counter] == '÷' || symbols[counter] == '×' || symbols[counter] == '-' || symbols[counter] == '+' || symbols[counter] == '='){
                 newBtn.style.backgroundColor = "#A9754F";
             }
-            newBtn.style.borderRadius = "4px";
+            
             newBtn.appendChild(btnText);
             container.appendChild(newBtn);
             counter++;
@@ -49,8 +70,8 @@ function divide(a,b){
 }
 
 function operate(symb, a, b){
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     switch(symb){
         case "+":   return add(a, b);
         case "-":   return sub(a, b);
@@ -65,16 +86,18 @@ function run(){
     let prevSym = "";
     let sym = "";
     let boolSym = false;
-
     let ans = "";
-    
     let screenText = "";
+
+    init();
+
     const screen = document.querySelector('#screen');
 
-    let btns = document.querySelector('#btn-container').childNodes;
-    btns = Array.from(btns);
+    let btns = Array.from(topContainer.childNodes);
+    btns.push(...Array.from(container.childNodes));
 
     for(let i=0;i<btns.length;i++){
+
         btns[i].addEventListener('mouseover', function(){
             btns[i].style.transform = "scale(1.1)";
         });
@@ -84,6 +107,7 @@ function run(){
         });
 
         btns[i].addEventListener('click', function(){
+
             btns[i].style.transform = "scale(0.9)";
             setTimeout(function(){
                 btns[i].style.transform = "scale(1)";
@@ -93,13 +117,18 @@ function run(){
                 //numbers, append to screen textContent
                 screenText = screen.textContent;
                 if(screenText == '0'){
+                    console.log(this);
                     screen.textContent = this.textContent;
                 }else{
                     if(boolSym == true){
                         screen.textContent = this.textContent;
                         boolSym = false;
                     }else{
-                        screen.textContent = screenText + this.textContent;
+                        if(screenText.length == 11){
+                            screen.textContent = screen.textContent;
+                        }else{
+                            screen.textContent = screenText + this.textContent;
+                        }
                     }
                 }
 
@@ -115,6 +144,10 @@ function run(){
                         screen.textContent = screen.textContent;
                     }else{
                         ans = operate(prevSym, prevNum, currNum);
+                        if(ans.toString().length > 10){
+                            let intLength = (Math.round(ans)).toString().length;
+                            ans = ans.toFixed(10-intLength);
+                        }
                         if(ans == undefined){
                             screen.textContent = screen.textContent;
                         }else if(ans == Infinity){
@@ -130,8 +163,8 @@ function run(){
                             }, 100);
                         }else{
                             screen.textContent = ans;
+                            boolSym = true;
                         }
-                        boolSym = false;
                     }
                 }else{
                     boolSym = true;
@@ -151,12 +184,34 @@ function run(){
                 }
                 
             }else if(this.style.backgroundColor == "rgb(190, 158, 201)"){
-                //c, set textContent back to zero and set nums, symb back to ""
-                screen.textContent = '0';
-                sym, prevSym = '';
-                currNum = null, prevNum = null;
-                boolSym = false;
-                ans = undefined;
+                //switched from clear button to period. 
+                if(screen.textContent == '0'){
+                    screen.textContent = '0.';
+                }else{
+                    screen.textContent = screen.textContent + '.';
+                }
+                
+            }else if(this.style.backgroundColor == "rgb(68, 56, 80)"){
+                //diff between cl/del buttons
+                if(this.textContent == 'C'){
+                    screen.textContent = '0';
+                    sym, prevSym = '';
+                    currNum = null, prevNum = null;
+                    boolSym = false;
+                    ans = undefined;
+                }else if(this.textContent == 'Del'){
+                    if( parseFloat(screen.textContent) < 10 ){
+                        screen.textContent = '0';
+                    }else{
+
+                        let tempText = screen.textContent;
+                        tempText = tempText.slice(0, tempText.length-1);
+
+                        screen.textContent = tempText;
+
+                    }
+                }
+
             }
 
         });
@@ -189,5 +244,8 @@ document.addEventListener('keydown', (event) =>{
         document.getElementById('×').click();
     }
 
+    if(event.key == 'Backspace'){
+        document.getElementById('Del').click();
+    }
 
 });
